2de03039eb143a4e354ae82446652da360f1fb98
Joseph DeWeese <rangerboy901@icloud.com>
2022-07-06 18:18:36 -0500

    bug extermination

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-window.alert("JS is connected");
+
+//window.alert("JS is connected");
+/*
+Joseph DeWeese
+5 July 2022
+Math Operators
+ */
+
+// Create number variable
+let num1 = 5+2;
+console.log("num1 is "+num1);
+
+let num2 = 10-6;
+console.log("num2 is "+num2);
+
 
 
 
